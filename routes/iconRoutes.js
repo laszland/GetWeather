9a8dd8364ac669f63936b03216d7e08dd0c3694a
Laszlo Roland Kiss <laszlo.roland.kiss@gmail.com>
2020-05-09 17:46:05 +0200

    style home screen, add daily forecast with real data

---
+++ routes/iconRoutes.js
@@ -0,0 +1,3 @@
+export const icons = {
+  '01d': 
+}
--- screens/home.js
+++ screens/home.js
@@ -19,6 +19,7 @@ import LocationItem from '../components/locationItem';
 import { getWeatherData } from '../services/weatherData';
 import CustomButton from '../components/button';
 import Header from '../components/header'
+const moment = require('moment')
 
 
 export default class Home extends React.Component {
@@ -41,9 +42,7 @@ export default class Home extends React.Component {
       hourly: [],
       daily: [],
     }
-    this.updateState();
   }
-  
   
   closeModal = async () => {
     const city = ['city', this.state.city];
@@ -83,9 +82,12 @@ export default class Home extends React.Component {
     this.setState({ modalOpen: true })
   }
 
+  componentDidMount() {
+    this.updateState();
+  }
   
   render() {
-    
+
     return (
           <ImageBackground source={require('../assets/backgrounds/weather-data.jpg')} style={styles.image}>
             <View style={styles.homeContainer}>
@@ -147,26 +149,31 @@ export default class Home extends React.Component {
                 <Text style={styles.cardTitle}>{this.state.city}</Text>
                 <Image source={require('../assets/icons/draw.png')} style={styles.icon}/>
               </TouchableOpacity>
-
-              <View>
-                <Text>{this.state.current.temp}</Text>
-                <View>
-                  <Text></Text>
+              
+              <TouchableOpacity style={styles.currentWeatherContainer}>
+                <Text style={styles.currentWeatherTemp}>{ parseInt(this.state.current.temp) }º</Text>
+                <View style={styles.iconAndTextContainer}>
+                  <Image source={require('../assets/icons/weather/01d.png')} style={styles.iconBig}/>
+                  <Text style={styles.currentDescription}>sunny</Text>
                 </View>
-              </View>
+              </TouchableOpacity>
 
               <FlatList
-                data={this.state.data}
+                data={this.state.daily}
                 style={styles.list}
-                renderItem={({ item }) => (
-                  <TouchableOpacity onPress={() => this.props.navigation.navigate('DayDetails', item)}>
-                    <View style={styles.cardContent}>
-                      <Text style={styles.cardTitle}>{item.day}</Text>
-                      <Text>{item.temp} ºC</Text>
-                      <Text>{item.description}</Text>
-                    </View>
-                  </TouchableOpacity >
-                )}
+                keyExtractor={item => item.dt.toString()}
+                renderItem={({ item }) => {
+                  const day = moment.unix(item.dt).format('dd')
+                  return (
+                    <View >
+                      <View style={styles.cardContent}>
+                        <Text style={styles.cardDay}>{day}</Text>
+                        <Text style={styles.cardTemp}>{parseInt(item.temp.min)}/{parseInt(item.temp.max)} ºC</Text>
+                        <Text style={styles.cardTitle}>{item.weather[0].icon}</Text>
+                      </View>
+                    </View >
+                )
+              }}
               />
             </View>
           </ImageBackground>
@@ -179,14 +186,15 @@ const styles = StyleSheet.create({
     flex: 1
   },
   cardContent: {
-    width: 200,
-    padding: 10,
-    borderWidth: 1,
-    borderRadius: 6,
-    borderColor: '#999',
+    width: Dimensions.get('window').width-24,
+    height: 68,
     marginTop: 20,
     marginHorizontal: 20,
-    justifyContent: 'center'
+    alignSelf: 'center',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'rgba(24, 100, 105, 0.5)',
+    flexDirection: 'row'
   },
   cardTitle: {
     fontFamily: 'nunito-extralight',
@@ -228,7 +236,7 @@ const styles = StyleSheet.create({
   },
   homeContainer: {
     flex: 1,
-    width: '100%',
+    width: Dimensions.get('window').width,
   },
   icon: {
     width: 14,
@@ -238,5 +246,52 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'center',
+  },
+  currentWeatherContainer: {
+    flexDirection: 'row',
+    height: 130,
+    width: Dimensions.get('window').width-24,
+    backgroundColor: 'rgba(24, 100, 105, 0.5)',
+    marginTop: 30,
+    padding: 0,
+    alignSelf: 'center',
+  },
+  currentWeatherTemp: {
+    fontFamily: 'nunito-extralight',
+    fontSize: 96,
+    color: '#FFFFFF',
+    textAlign: 'left',
+    textAlignVertical: 'center',
+    lineHeight: 130,
+    paddingLeft: 12
+  },
+  iconAndTextContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    //backgroundColor: 'pink'
+  },
+  iconBig: {
+    width: 60,
+    height: 60
+  },
+  currentDescription: {
+    fontFamily: 'nunito-bold',
+    fontSize: 14,
+    marginTop: 5,
+    color: '#FFFFFF'
+  },
+  cardDay: {
+    fontFamily: 'nunito-extralight',
+    fontSize: 30,
+    color: '#FFFFFF',
+    textAlign: 'left',
+    paddingLeft: 12,
+  },
+  cardTemp: {
+    fontFamily: 'nunito-extralight',
+    fontSize: 45,
+    color: '#FFFFFF',
   },
 });
